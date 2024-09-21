eab589c55791099651525683cf70e6cf60afcc17
vinita <vinitamenaria2004@gmail.com>
2024-09-21 15:26:06 +0530

    card

---
+++ REACT/vite-project/src/components/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+        <p id='user-name'>Vinita Menaria</p>
+        <img id ="user-img"src=''></img>
+         <p id='user-desc'></p>
+    </div>
+  )
+}
+export default UserCard
